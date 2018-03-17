94fcf3fcf5405e2f61138ea74d4f621256a30232
Hector Flores <hectorflores19z@gmail.com>
2018-03-17 00:11:57 -0500

    Started added angular to html

---
+++ ExpressBuy/app/expressbuy.js
@@ -0,0 +1,1 @@
+var expressBuyApp = angular.module('expressBuyApp', []);
---
+++ ExpressBuy/app/expressbuy/ebController.js
@@ -0,0 +1,7 @@
+expressBuyApp.controller("ebController", function ($scope) {
+    $scope.products = [
+        { name: 'SSD Drives' },
+        { name: 'Monitors' },
+        { name: 'Graphics Cards' }
+    ];
+});
